8b7315a0482b003276296b3c87615ea0316b5c0c
Bernard Mulaw <bernard@dhcp-wifi-8021x-155-41-17-113.bu.edu>
2021-09-13 18:05:12 -0400

    notes for launch.sh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# will use ProductionConfig bc its being deployed
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
-
+# don't need to init a db
 flask db migrate
 flask db upgrade
 #flask run --host=0.0.0.0 --port=5000
